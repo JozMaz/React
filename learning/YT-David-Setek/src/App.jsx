f30c46d453962573f9b69e8a571e450fbc05cde5
Jozue Dan Mazur <150478906+JozMaz@users.noreply.github.com>
2025-06-12 12:58:15 +0200

    Add files via upload

---
+++ learning/YT-David-Setek/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Project1 from "./components/Project1";
+import Project2API from "./components/Project2API";
+import Project3ISS from "./components/Project3ISS";
+import Project4ISSLoading from "./components/Project4ISSLoading";
+import Project5ISSLoading from "./components/Project5Films";
+import Project6ShowContent from "./components/Project6ShowContent";
+import Project7MoviesSlider from "./components/Project7MoviesSlider";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
