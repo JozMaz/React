95fea28a44be4d7c802815cc73f4cfebcd25dfed
Jozue Dan Mazur <150478906+JozMaz@users.noreply.github.com>
2025-06-26 09:04:30 +0200

    Add files via upload

---
+++ learning/YT-David-Setek/src2/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Project1Input from "./components/Project1Inputs";
+import Project2Generator from "./components/Project2Generator";
+import Project3Ref from "./components/Project3Ref";
+import Project4Reducer from "./components/Project4Reducer";
+import Project5Context from "./components/Project5Context";
+import Project6CustomHooks from "./components/Project6CustomHooks";
+import Project7ValidatingProps from "./components/Project7ValidatingProps";
+import Project8SearchFiltering from "./components/Project8SearchFiltering";
+
+const App = () => {
+  return (
+    <>
+      <Project8SearchFiltering />
+    </>
+  );
+};
+
+export default App;
